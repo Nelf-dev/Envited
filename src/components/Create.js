49056fe3868767cf1d84ec5aaf4f90a2036a6047
Nelson Feng <nelsonfeng98@gmail.com>
2022-09-29 07:33:26 +1000

    Boiler Plate and uneccessary files removed

---
+++ src/components/Create.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Create = () => {
+    return (
+        <div>
+            This is the Create Page
+        </div>
+    );
+}
+
+export default Create;
---
+++ src/components/Event.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Event = () => {
+    return (
+        <div>
+            This is the event Page
+        </div>
+    );
+}
+
+export default Event;
