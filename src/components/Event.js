0e8c2021acc84e55d1e56bbe2c28cbd9d3b55abf
Nelson Feng <nelsonfeng98@gmail.com>
2022-09-29 09:38:42 +1000

    base css for event page completed

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import birthday from '../img/birthday.png';
+import { FaCalendar } from 'react-icons/fa';
+import { GrFormNext, GrLocation } from 'react-icons/gr';
+
 const Event = () => {
     return (
         <div>
-            This is the event Page
+            <img className="birthday" src={birthday} alt="Birthday Cake"/>
+            <h1 className="eventtitle">Birthday Bash</h1>
+            <p className="host">Hosted by <span className="bold">Elysia</span></p>
+            
+            <div className="details">
+                <div className="iconAndInfo">
+                    <div className="icon">
+                        <FaCalendar />
+                    </div>
+                    <div className="info">
+                        <p className="bold">18 August 6:00PM</p>
+                        <p>to <span className="bold">19 August 1:00PM UTC +10</span></p>
+                    </div>
+                </div>
+                <div className="next">
+                    <GrFormNext />
+                </div>
+            </div>
+
+            <div className="details">
+                <div className="iconAndInfo">
+                    <div className="icon">
+                        <GrLocation />
+                    </div>
+                    <div className="info">
+                        <p className="bold">Street Name</p>
+                        <p>Suburb, State, Postcode</p>
+                    </div>
+                </div>
+                <div className="next">
+                    <GrFormNext />
+                </div>
+            </div>
             <Link to="/"> <button>Home</button> </Link>
         </div>
     );
